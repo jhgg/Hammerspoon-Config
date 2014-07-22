8c8115490e26f93c584a3a2c2baf79e33e9749ab
Jake <jh@tsl.io>
2014-07-22 12:24:05 -0400

    Don't allow empty titles (+ reuse title var)

--- modules/app_selector.lua
+++ modules/app_selector.lua
@@ -3,12 +3,19 @@ local geom = import('utils/geometry')
 
 local function module_init()
     local function match_data_source()
-        return fnutils.map(window.allwindows(), function(window)
-            return {
-                string = window:application():title() .. ' - ' .. window:title(),
-                window = window
-            }
-        end)
+        local results = {}
+
+        for _, win in ipairs(window.allwindows()) do
+            local title = win:title()
+
+            if title:len() > 0 then
+                table.insert(results, {
+                    string = win:application():title() .. ' - ' .. title,
+                    window = win
+                })
+            end
+        end
+        return results
     end
 
     local function match_selected(match)
@@ -18,12 +25,11 @@ local function module_init()
             local center = geom.rect_midpoint(match.window:frame())
             mouse.set(center)
         end
-
     end
 
     local matcher = match_dialgoue(match_data_source, match_selected)
 
-    hotkey.bind(config:get('app_selector.mash', {"ctrl"}), config:get('app_selector.key', "tab"), function()
+    hotkey.bind(config:get('app_selector.mash', { "ctrl" }), config:get('app_selector.key', "tab"), function()
         matcher:show()
     end)
 end
